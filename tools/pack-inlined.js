c309b5b0800a48a1e123cd08a26dc764069606e2
marco <Marco.Stahl@acrolinx.com>
2018-07-25 09:29:38 +0200

    DEV-13832 sidebar-sdk-js should include and use sidebar-startpage by default
    * Replaced ACROLINX_SERVER_SELECTOR_INLINED in dist/inlined/acrolinx-sidebar-startpage-inlined.js with ACROLINX_STARTPAGE_INLINED_HTML in dist/index.js
    * ACROLINX_STARTPAGE_INLINED_HTML inlines now also the fonts

--- tools/pack-inlined.js
+++ tools/pack-inlined.js
@@ -4,8 +4,7 @@ function escapeClosingScriptTag(html) {
   return html.replace('</script>', '<\\/script>');
 }
 
-const IMPORT_FONTS_HTML = "<style>@import url('https://fonts.googleapis.com/css?family=Roboto');</style>";
-
 const fileContent = fs.readFileSync('tmp/dist-inlined.html', 'utf-8');
-const fileContentWithFonts = fileContent.replace('§INCLUDE_FONTS', IMPORT_FONTS_HTML);
-console.log("var ACROLINX_SERVER_SELECTOR_INLINED = " + escapeClosingScriptTag(JSON.stringify(fileContentWithFonts)) + ";");
+
+// If you change this variable name, you might also want to change dist/index.d.ts.
+console.log("export var ACROLINX_STARTPAGE_INLINED_HTML = " + escapeClosingScriptTag(JSON.stringify(fileContent)) + ";");
