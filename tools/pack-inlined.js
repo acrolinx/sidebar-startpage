8c945fe0f426b0c7364dff2338267cb8902dc818
marco <Marco.Stahl@acrolinx.com>
2018-07-25 11:22:03 +0200

    Release version 2.0.1
    * DEV-13832 sidebar-sdk-js should include and use sidebar-startpage by default: Fixed export of ACROLINX_STARTPAGE_INLINED_HTML

--- tools/pack-inlined.js
+++ tools/pack-inlined.js
@@ -7,4 +7,4 @@ function escapeClosingScriptTag(html) {
 const fileContent = fs.readFileSync('tmp/dist-inlined.html', 'utf-8');
 
 // If you change this variable name, you might also want to change dist/index.d.ts.
-console.log("export var ACROLINX_STARTPAGE_INLINED_HTML = " + escapeClosingScriptTag(JSON.stringify(fileContent)) + ";");
+console.log("exports.ACROLINX_STARTPAGE_INLINED_HTML = " + escapeClosingScriptTag(JSON.stringify(fileContent)) + ";");
