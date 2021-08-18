1e5c9449c3c16c444cbd775422108475a8f597a2
Ahmed Abdelsalam <ahmed.abdelsalam@acrolinx.com>
2021-08-18 12:45:26 +0200

    DEV-27141 setting up npm token

---
+++ gitlab-build-ci.sh
@@ -0,0 +1,15 @@
+set -ex
+
+SELECTOR_VERSION=`cat package.json | grep "version\":" | cut -d'"' -f4`
+
+# SIDEBAR_VERSION_SHORT=$(printf "%s\n" "${SIDEBAR_VERSION?}" | cut -d . -f 1-2)
+CLASSIFIER=-SNAPSHOT
+
+# # prod is triggered on release tag!
+# if [ "$stage" = "prod" ]; then
+#   	CLASSIFIER=.$CI_PIPELINE_IID
+# fi
+
+mvn versions:set -DnewVersion=$SELECTOR_VERSION$CLASSIFIER -DgenerateBackupPoms=false
+
+mvn deploy -DpomFile=pom.xml -s ci_settings.xml
