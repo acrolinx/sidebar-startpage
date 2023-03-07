6d366ff8308ed9116f110deadcf76b2c8e98971b
Sascha Günther <sguenther@mscloud.consulting>
2023-03-07 09:41:57 +0100

    set copyright from present year

--- src/utils/externalLinks.ts
+++ src/utils/externalLinks.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-present Acrolinx GmbH
+ * Copyright 2023-present Acrolinx GmbH
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
