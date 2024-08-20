42769de9e7c8cf40087491e9fc86bc7d3091b985
Abhijeet Narvekar <abhijeet.narvekar@acrolinx.com>
2024-08-20 12:21:59 +0000

    fix: inline export syntax

--- tools/pack-inlined.js
+++ tools/pack-inlined.js
@@ -5,4 +5,4 @@ const escapeClosingScriptTag = html => html.replace('</script>', '<\\/script>');
 const fileContent = readFileSync('dist/dist-inline/index.html', 'utf-8');
 
 // If you change this variable name, you might also want to change dist/index.d.ts.
-console.log(`exports.ACROLINX_STARTPAGE_INLINED_HTML = ${escapeClosingScriptTag(JSON.stringify(fileContent))};`);
+console.log(`export const ACROLINX_STARTPAGE_INLINED_HTML = ${escapeClosingScriptTag(JSON.stringify(fileContent))};`);
